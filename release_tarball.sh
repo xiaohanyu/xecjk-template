01667f1272a7c5abf097078b15282bf15dc95c79
Xiao Hanyu <xiaohanyu1988@gmail.com>
2011-05-16 09:35:34 +0800

    add GPL header to main.tex improve tarball release script by cleanning uncessary files before release

--- release_tarball.sh
+++ release_tarball.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 
+# software version
 VERSION=$1
+
+# clean uncessary temporary files
+make -f tex/Makefile cleanall
 
 tar czvf xecjk-template-${VERSION}.tar.gz tex
 tar czvf xecjk-template-all-${VERSION}.tar.gz CHANGES README LICENSE IDEs scripts tex
